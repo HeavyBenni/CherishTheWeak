4416c69bb372f55ba648e5321c6a1795bca2d90f
HeavyBenni <122980906+HeavyBenni@users.noreply.github.com>
2023-08-29 11:11:47 +0200

    Demo

---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MainHome extends StatefulWidget {
+  const MainHome({super.key});
+
+  @override
+  State<MainHome> createState() => _MainHomeState();
+}
+
+class _MainHomeState extends State<MainHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('CTW GANG'),
+    );
+  }
+}
