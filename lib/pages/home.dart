64052ed3326b9e692f1f0f7013383938214336d9
ChristianKode <christian.storhaug05@gmail.com>
2023-08-29 11:26:16 +0200

    Added NavBar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:cherishtheweak/widget/navbar.dart';
 import 'package:flutter/material.dart';
 
 class MainHome extends StatefulWidget {
@@ -10,8 +11,14 @@ class MainHome extends StatefulWidget {
 class _MainHomeState extends State<MainHome> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('CTW GANG'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        actions: [NavBar()],
+      ),
+      body: const Center(
+        child: Text('CTW GANG'),
+      ),
     );
   }
 }
