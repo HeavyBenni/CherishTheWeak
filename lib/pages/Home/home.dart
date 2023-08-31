e675a5d98c0029ec82bb07e1c7484ff02b70679f
ChristianKode <christian.storhaug05@gmail.com>
2023-08-31 10:01:59 +0200

    Animation added

--- lib/pages/Home/home.dart
+++ lib/pages/Home/home.dart
@@ -1,24 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:cherishtheweak/theme/theme.dart';
 import 'package:cherishtheweak/widget/navbar/navbar.dart';
-import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MainHome extends StatefulWidget {
-  const MainHome({super.key});
+  const MainHome({Key? key}) : super(key: key);
 
   @override
   State<MainHome> createState() => _MainHomeState();
 }
 
 class _MainHomeState extends State<MainHome> {
-  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+  double opacity = 0.0; // Initial opacity value
+
+  @override
+  void initState() {
+    super.initState();
+    // Start the fade-in animation when the widget is first built
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      setState(() {
+        opacity = 1.0; // Set opacity to 1 for a full fade-in effect
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: NavBar(logoName: 'Cherish The Weak'),
-      body:  Center(
-        child: Text('CTW GANG', style: AppTheme.headLineLarge,),
+    return AnimatedOpacity(
+      duration: Duration(seconds: 2),
+      opacity: opacity,
+      child: Scaffold(
+        appBar: NavBar(logoName: 'Cherish The Weak'),
+        body: Center(
+          // Wrap the Text widget with AnimatedOpacity
+          child: Text('CTW GANG', style: AppTheme.headLineLarge),
+        ),
       ),
     );
   }
